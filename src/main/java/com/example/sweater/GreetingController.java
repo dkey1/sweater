2f5798409fd80615e72e2832016b3db607b73c36
dkey1 <dkey.mke@gmail.com>
2019-03-24 00:51:04 +0200

    start adding mvc

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -21,20 +21,19 @@ public class GreetingController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(Map<String, Object> model, @RequestParam String text, @RequestParam String tag) {
         Message message = new Message(text, tag);
         messageRepo.save(message);
